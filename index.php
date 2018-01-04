264332e86505fc11a796af7d309e7daff5b58c92
harits-git <game.harits@gmail.com>
2018-01-04 12:52:47 +0700

    'added index file'

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Experiment - Input data Antrean</title>
+</head>
+<body>
+<form>
+	<h1>Masukkan data fans yang antri ketemu artis</h1>
+</form>
+</body>
+</html>
